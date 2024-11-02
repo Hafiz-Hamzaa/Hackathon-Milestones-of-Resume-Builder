cb9ceb51b815ae06594115eeff9f56fd508e4d5c
Hafiz Hamza <161071471+Hafiz-Hamzaa@users.noreply.github.com>
2024-11-02 09:27:14 -0700

    Add files via upload

---
+++ Milestone 4/script.js
@@ -0,0 +1,31 @@
+var form = document.getElementById('resume-form');
+var resumeDisplayElement = document.getElementById('resume-preview');
+// HANDLING FORM SUBMISSION
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // prevent page reload
+    // Collection Input 
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var degree = document.getElementById('degree').value;
+    var institute = document.getElementById('institute').value;
+    var duration = document.getElementById('duration').value;
+    var skills = document.getElementById('skills').value;
+    var experience = document.getElementById('experience').value;
+    // const profileImage = (document.getElementById('profile-image') as HTMLInputElement).files[0]
+    var profileImageInput = document.getElementById('profile-image');
+    var profileImage = profileImageInput.files && profileImageInput.files[0];
+    if (!profileImage) {
+        console.error('No profile image selected');
+        return;
+    }
+    // Generate resume content 
+    var resumeHTML = "\n<div class=\"resume-container\">\n    <h1>Dynamic and Editable Resume</h1>\n    <div class=\"profile-section\">\n        <img src=\"".concat(URL.createObjectURL(profileImage), "\" alt=\"Profile Image\" width=\"100\" height=\"100\">\n        <h2><span contenteditable=\"true\">").concat(name, "</span></h2>\n        <p><span contenteditable=\"true\">").concat(email, " | ").concat(phone, " </span></p>\n    </div>\n    <hr>\n    <section>\n        <h3>Education</h3>\n        <ul>\n            <li><b>Degree:</b><span contenteditable=\"true\"> ").concat(degree, "</span></li>\n            <li><b>Institute:</b><span contenteditable=\"true\"> ").concat(institute, "</span></li>\n            <li><b>Duration:</b> <span contenteditable=\"true\">").concat(duration, "</span></li>\n        </ul>\n    </section>\n    <section>\n        <h3>Skills</h3>\n        <ul>\n            <span contenteditable=\"true\">").concat(skills.split(',').map(function (skill) { return "<li>".concat(skill.trim(), "</li>"); }).join(''), "</span>\n        </ul>\n    </section>\n    <section>\n        <h3>Experience</h3>\n        <p contenteditable=\"true\">").concat(experience, "</p>\n    </section>\n</div>\n\n");
+    // Display Generated Resume
+    if (resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }
+    else {
+        console.error('The resume display element is missing.');
+    }
+});
---
+++ Milestone 4/script.ts
@@ -0,0 +1,64 @@
+let form = document.getElementById('resume-form') as HTMLFormElement;
+let resumeDisplayElement = document.getElementById('resume-preview') as HTMLDivElement
+// HANDLING FORM SUBMISSION
+form.addEventListener('submit' , (event:Event)=>{
+    event.preventDefault(); // prevent page reload
+
+    // Collection Input 
+    const name = (document.getElementById('name') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const phone = (document.getElementById('phone') as HTMLInputElement).value
+    const degree = (document.getElementById('degree') as HTMLInputElement).value
+    const institute = (document.getElementById('institute') as HTMLInputElement).value
+    const duration = (document.getElementById('duration') as HTMLInputElement).value
+    const skills = (document.getElementById('skills') as HTMLInputElement).value
+    const experience = (document.getElementById('experience') as HTMLInputElement).value
+    // const profileImage = (document.getElementById('profile-image') as HTMLInputElement).files[0]
+    const profileImageInput = document.getElementById('profile-image') as HTMLInputElement;
+const profileImage = profileImageInput.files && profileImageInput.files[0];
+
+if (!profileImage) {
+    console.error('No profile image selected');
+    return;
+}
+
+    // Generate resume content 
+
+const resumeHTML = `
+<div class="resume-container">
+    <h1>Dynamic and Editable Resume</h1>
+    <div class="profile-section">
+        <img src="${URL.createObjectURL(profileImage)}" alt="Profile Image" width="100" height="100">
+        <h2><span contenteditable="true">${name}</span></h2>
+        <p><span contenteditable="true">${email} | ${phone} </span></p>
+    </div>
+    <hr>
+    <section>
+        <h3>Education</h3>
+        <ul>
+            <li><b>Degree:</b><span contenteditable="true"> ${degree}</span></li>
+            <li><b>Institute:</b><span contenteditable="true"> ${institute}</span></li>
+            <li><b>Duration:</b> <span contenteditable="true">${duration}</span></li>
+        </ul>
+    </section>
+    <section>
+        <h3>Skills</h3>
+        <ul>
+            <span contenteditable="true">${skills.split(',').map(skill => `<li>${skill.trim()}</li>`).join('')}</span>
+        </ul>
+    </section>
+    <section>
+        <h3>Experience</h3>
+        <p contenteditable="true">${experience}</p>
+    </section>
+</div>
+
+`;
+       // Display Generated Resume
+    if(resumeDisplayElement){
+        resumeDisplayElement.innerHTML = resumeHTML; 
+    } else {
+        console.error('The resume display element is missing.')
+    }
+
+})
